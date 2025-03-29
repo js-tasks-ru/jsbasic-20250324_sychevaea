b8998002235bcc9a8a08970b896d261e40652471
69IstominaEA <69IstominaEA@region.cbr.ru>
2025-03-29 10:04:04 +0300

    Task-4 добавлена функция checkSpam

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,14 @@
 function checkSpam(str) {
-  // ваш код...
+  if (!str) return false;
+
+  str = str.toLowerCase();
+
+  const value_alpha = '1xBet'.toLowerCase();
+  const value_beta = 'XXX'.toLowerCase();
+
+  if (str.includes(value_alpha)) return true;
+
+  if(str.includes(value_beta)) return true;
+
+  return false;
 }
