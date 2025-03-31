93389ede7ece44d5648edd823c710afbd0368d6e
69IstominaEA <69IstominaEA@region.cbr.ru>
2025-03-31 10:42:05 +0300

    Task-1 переделана функция factorial()

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,11 +1,11 @@
 function factorial(n) {
-  if (n === 0) return 1;
-  if (n === 1) return 1;
+  if (n == 0) return 1;
+  if (n == 1) return 1;
+
   let result = 1;
   let delta = 0;
-  while(n - delta) {
-    result *= (n - delta);
-    delta += 1;
+  while(n-delta) {
+    result *= (n -delta);
+    delta++;
   }
-  return result;
 }
