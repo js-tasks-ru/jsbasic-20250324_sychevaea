9b7bdf2bb2fa475e90962b173d7c56558d722913
69IstominaEA <69IstominaEA@region.cbr.ru>
2025-04-04 19:11:53 +0300

    Task 3.3 Добавлена функция camelize

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,11 @@
 function camelize(str) {
-  // ваш код...
+  if (!str) return false;
+
+  let arr = str.split('-');
+
+  let updated_arr = arr.map((item, index) => {
+    return index == 0 ? item : item[0].toUpperCase() + item.slice(1);
+  })
+
+  return updated_arr.join('');
 }
