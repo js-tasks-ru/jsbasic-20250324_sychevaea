019349aa68b1c62cf392629223b46ecc3d67f702
69IstominaEA <69IstominaEA@region.cbr.ru>
2025-04-09 14:56:07 +0300

    Task 4.2 правки по результатам тестирования

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,9 +1,8 @@
 function makeDiagonalRed(table) {
-  const tbl = document.body.firstElementChild;
-
-  Array.from(tbl.rows).forEach((tr, index) => {
+  Array.from(table.rows).forEach((tr, index) => {
     Array.from(tr.cells).forEach((cell, i, arr) => {
       arr[index].style.backgroundColor = 'red';
     })
   });
+  return table;
 }
