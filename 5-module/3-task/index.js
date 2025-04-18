c6f7c65f9e0314919aefc513fc945654b4d5549c
69IstominaEA <69IstominaEA@region.cbr.ru>
2025-04-18 19:44:18 +0300

    Task 5.3 добавлена функция initCarousel

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,33 @@
 function initCarousel() {
-  // ваш код...
+  let left = document.querySelector('.carousel__arrow_left');
+  let right = document.querySelector('.carousel__arrow_right');
+  let elem = document.querySelector('.carousel__inner');
+
+  const width = elem.offsetWidth;
+
+  let count = 0;
+
+  let position = 0;
+
+  left.style.display = 'none';
+
+  right.onclick = function(e){
+    position -= width;
+    elem.style.transform = `translateX(${position}px)`;
+    count += 1;
+    if (count == 3){
+      right.style.display = 'none';
+    }
+    left.style.display = '';
+  }
+
+  left.onclick = function(e){
+    position += width;
+    elem.style.transform = `translateX(${position}px)`;
+    count -= 1;
+    if (count == 0){
+      left.style.display = 'none';
+    }
+    right.style.display = '';
+  }
 }
